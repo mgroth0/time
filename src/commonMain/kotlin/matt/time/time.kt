20cc3a273f2e898aaa1cf88eaf2e35e446211a2b
mgroth0 <mgroth49@gmail.com>
2023-04-01 19:48:15 -0400

    autocommit

--- src/commonMain/kotlin/matt/time/time.kt
+++ src/commonMain/kotlin/matt/time/time.kt
@@ -5,10 +5,13 @@ import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.Serializable
+import matt.lang.function.Produce
 import matt.lang.unixTime
+import matt.model.flowlogic.controlflowstatement.ControlFlow
 import matt.model.op.convert.Converter
 import matt.time.AMOrPM.AM
 import matt.time.AMOrPM.PM
+import matt.time.dur.sleep
 import kotlin.jvm.JvmInline
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.days
@@ -25,35 +28,36 @@ enum class AMOrPM { AM, PM }
 
 
 val LocalDateTime.minPart
-  get() = when {
-	minute < 10 -> "0$minute"
-	else        -> minute
-  }
+    get() = when {
+        minute < 10 -> "0$minute"
+        else -> minute
+    }
 val LocalDateTime.halfDayHour
-  get() = when (val h = hour%12) {
-	0    -> 12
-	else -> h
-  }
+    get() = when (val h = hour % 12) {
+        0 -> 12
+        else -> h
+    }
 val LocalDateTime.amOrPm get() = if (hour >= 13) PM else AM
 fun UnixTime.toLocalDateTime() = toInstant().toLocalDateTime()
 fun Instant.toLocalDateTime() = toLocalDateTime(TimeZone.currentSystemDefault())
 fun UnixTime.toInstant() = Instant.fromEpochMilliseconds(duration.inWholeMilliseconds)
 
 
-const val MINUTE_MS: Int = 60*1000
-const val HOUR_MS: Int = 3600*1000
+const val MINUTE_MS: Int = 60 * 1000
+const val HOUR_MS: Int = 3600 * 1000
 
 fun Duration.toUnixTime() = UnixTime(this)
 
 
 @Serializable
-@JvmInline value class UnixTime(val duration: Duration = unixTime()): Comparable<UnixTime> {
+@JvmInline
+value class UnixTime(val duration: Duration = unixTime()) : Comparable<UnixTime> {
 
-  override fun compareTo(other: UnixTime) = duration.compareTo(other.duration)
-  operator fun minus(other: UnixTime) = duration - other.duration
-  operator fun plus(other: UnixTime) = duration + other.duration
-  operator fun minus(other: Duration) = UnixTime(duration - other)
-  operator fun plus(other: Duration) = UnixTime(duration + other)
+    override fun compareTo(other: UnixTime) = duration.compareTo(other.duration)
+    operator fun minus(other: UnixTime) = duration - other.duration
+    operator fun plus(other: UnixTime) = duration + other.duration
+    operator fun minus(other: Duration) = UnixTime(duration - other)
+    operator fun plus(other: Duration) = UnixTime(duration + other)
 }
 
 
@@ -63,19 +67,19 @@ fun DurationUnit.of(amount: Long) = amount.toDuration(this)
 fun DurationUnit.of(amount: Int) = amount.toDuration(this)
 
 
-object MilliSecondDurationConverter: Converter<Duration, Double> {
-  override fun convertToB(a: Duration): Double {
-	return a.inWholeMilliseconds.toDouble()
-  }
+object MilliSecondDurationConverter : Converter<Duration, Double> {
+    override fun convertToB(a: Duration): Double {
+        return a.inWholeMilliseconds.toDouble()
+    }
 
-  override fun convertToA(b: Double): Duration {
-	return b.milliseconds
-  }
+    override fun convertToA(b: Double): Duration {
+        return b.milliseconds
+    }
 
 }
 
 fun Duration.remMillis(d: Duration): Duration {
-  return inWholeMilliseconds.rem(d.inWholeMilliseconds).milliseconds
+    return inWholeMilliseconds.rem(d.inWholeMilliseconds).milliseconds
 }
 
 val ONE_NANOSECOND = 1.nanoseconds
@@ -87,15 +91,38 @@ val ONE_HOUR = 1.hours
 val ONE_DAY = 1.days
 
 val Duration.largestFullUnit
-  get() = when {
-	this == Duration.ZERO  -> null
-	this > ONE_DAY         -> DurationUnit.DAYS
-	this > ONE_HOUR        -> DurationUnit.HOURS
-	this > ONE_MINUTE      -> DurationUnit.MINUTES
-	this > ONE_SECOND      -> DurationUnit.SECONDS
-	this > ONE_MILLISECOND -> DurationUnit.MILLISECONDS
-	this > ONE_MICROSECOND -> DurationUnit.MICROSECONDS
-	this >= ONE_NANOSECOND -> DurationUnit.NANOSECONDS
-	else                   -> error("could not figure out largestFullUnit for ${this}")
-  }
+    get() = when {
+        this == Duration.ZERO -> null
+        this > ONE_DAY -> DurationUnit.DAYS
+        this > ONE_HOUR -> DurationUnit.HOURS
+        this > ONE_MINUTE -> DurationUnit.MINUTES
+        this > ONE_SECOND -> DurationUnit.SECONDS
+        this > ONE_MILLISECOND -> DurationUnit.MILLISECONDS
+        this > ONE_MICROSECOND -> DurationUnit.MICROSECONDS
+        this >= ONE_NANOSECOND -> DurationUnit.NANOSECONDS
+        else -> error("could not figure out largestFullUnit for ${this}")
+    }
 
+
+enum class TimeoutLoopResult { TIMEOUT, BROKE }
+enum class StartWith { INTERVAL, OP }
+
+fun timeoutLoop(
+    interval: Duration,
+    startWith: StartWith,
+    timeout: Duration,
+    op: Produce<ControlFlow>
+): TimeoutLoopResult {
+    val timeoutEnd = UnixTime() + timeout
+    var result = TimeoutLoopResult.BROKE
+    if (startWith == StartWith.INTERVAL) sleep(interval)
+    while (true) {
+        if (UnixTime() > timeoutEnd) {
+            result = TimeoutLoopResult.TIMEOUT
+            break
+        }
+        if (op() == ControlFlow.BREAK) break
+        sleep(interval)
+    }
+    return result
+}
