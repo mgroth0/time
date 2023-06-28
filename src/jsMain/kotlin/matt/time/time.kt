cd381f1c5f94488622247f5163cbf56e7da6ffcf
mgroth0 <mgroth49@gmail.com>
2023-06-28 17:37:13 -0400

    changed 2 files

--- src/jsMain/kotlin/matt/time/time.kt
+++ src/jsMain/kotlin/matt/time/time.kt
@@ -1,7 +1,6 @@
 package matt.time
 
 import kotlinx.datetime.toKotlinInstant
-import matt.time.toLocalDateTime
 import kotlin.js.Date
 
 actual fun nowKotlinDateTime() = Date().toKotlinInstant().toLocalDateTime()
